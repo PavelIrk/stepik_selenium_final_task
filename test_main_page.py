6fef12f65946587234a7c460ad5da4f9487b0244
PavelIrk <sviridpavel@mail.ru>
2022-03-26 06:24:24 +0800

    added method should_be_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -7,5 +7,7 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                     
     page.go_to_login_page()
     page.should_be_login_link()
+    page = LoginPage(browser, link)
+    page.should_be_login_page()
 
 
