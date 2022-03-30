f745a23949418f5a68ff0ce2996716f58aee1348
PavelIrk <sviridpavel@mail.ru>
2022-03-31 03:45:40 +0800

    added two tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 import pytest
+import time
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -15,9 +16,29 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)    
     page.open()
     page.should_be_promo_link()
+    page.should_not_be_success_message()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
+    time.sleep(1)
     page.should_be_message_about_adding()
+
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+
+
+
 
     
     
